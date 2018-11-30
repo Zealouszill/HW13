e50fb2137589f2c9d75723dac02b075ed51e3a55
Zealouszill <rubix.this@gmail.com>
2018-11-30 11:00:08 -0700

    Before Jonathan messed with code

---
+++ HW11Xamarain/HW11Xamarain/ViewModelLocator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharedLogic.ViewModel;
+
+
+namespace HW11Xamarain
+{
+    public class ViewModelLocator
+    {
+        private readonly MainViewModel _viewModel = new MainViewModel();
+
+        public MainViewModel Main
+        {
+            get { return _viewModel; }
+        }
+    }
+}
