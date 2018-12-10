ecfa79f25bd0fe9036a1df38b11c73ab0d5b8def
Zealouszill <rubix.this@gmail.com>
2018-12-10 10:52:45 -0700

    Push Everything back up

---
+++ SharedLogic/OneToTenValidation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace SharedLogic
+{
+    public class OneToTenValidation : ValidationRule
+    {
+        public OneToTenValidation() { }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int tempNumber;
+            int.TryParse(value.ToString(), out tempNumber);
+
+            try
+            {
+                if(tempNumber > 10 || tempNumber < 1)
+                {
+
+                }
+            } catch (Exception e)
+            {
+                return new ValidationResult(false, "Please enter a number between " + " - " + ".");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
